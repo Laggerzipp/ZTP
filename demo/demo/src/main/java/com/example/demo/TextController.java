95bfd7a8a7c3fcbbcab06fe4a820b324d213dbe8
Laggerzipp <127037347+Laggerzipp@users.noreply.github.com>
2024-10-19 11:27:15 +0200

    feature: add text counter (#2)
    Co-authored-by: Student Domena <student@WIiT.local>

---
+++ demo/demo/src/main/java/com/example/demo/TextController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/count")
+    public int countCharacters(@RequestParam String text) {
+        return text.length();
+    }
+}
+
+
+
+
